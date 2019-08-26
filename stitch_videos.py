b87dd8d4049f7a3db178d3410f7c4286573e7920
Matija8 <matijanme@gmail.com>
2019-08-26 21:27:00 +0200

    Stitch video works now

--- stitch_videos.py
+++ stitch_videos.py
@@ -11,22 +11,18 @@ def main():
         exit(f"Must have at least 2 arguments! {n_of_arguments-1} given.")
 
     files = sys.argv[1:]
-    #print(files)
 
     txt = "TMP - " + files[0] + ".txt"
     output = "STITCH - " + files[0]
 
     f = open(txt , "w+")
     for i in range(n_of_arguments-1):
-        #print("file '" + os.path.abspath(files[i]) + "' \n")
         f.write("file '" + os.path.abspath(files[i]) + "' \n")
-    #f.flush()
     f.close()
 
     cmd = "ffmpeg -f concat -safe 0 -i -c copy".split()
     cmd.insert(6, txt)
     cmd.append(output)
-    #print(cmd)
 
     start = time.time()
     print("\nRunning ffmpeg...\n")
