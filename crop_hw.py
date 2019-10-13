c596f02ead797432fe9af239255b85d3f7ddc502
Matija8 <matijanme@gmail.com>
2019-10-14 00:31:49 +0200

    Worked on crop. Errors pending.

--- crop_hw.py
+++ crop_hw.py
@@ -1,28 +1,29 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-
-#Crop height/width of a video
-#Reference: https://video.stackexchange.com/questions/4563/how-can-i-crop-a-video-with-ffmpeg
 import sys
-
+import subprocess
 
 
 def main():
-    #ffmpeg -i in.mp4 -filter:v "crop=out_w:out_h:x:y" out.mp4
     n_of_arguments = len(sys.argv)
     if n_of_arguments != 2:
         exit(f"Must have 1 argument (File to crop)! {n_of_arguments-1} given.")
+    in_file = sys.argv[1]
     print("Insert width and height for the output: ")
     out_w, out_h = take_two_ints()
-    print(out_w, out_h)
+    print("Insert start x and y coords: ")
+    x, y = take_two_ints()
+    #bash command
+    cmd = f'ffmpeg -i -filter:v "crop={out_w}:{out_h}:{x}:{y}"'.split()
+    cmd.insert(2, in_file)
+    cmd.append("CROPPED - " + in_file)
+    subprocess.run(cmd)
     
         
 
 def take_two_ints():
-    #practice: take_n_ints (recursion: take_n-1_ints...)
     try:
         first_line = input().split() #performance? Go to only second whitespace?
-        print("first line debug: " + str(first_line))
         if len(first_line) < 1:
             raise(ValueError)
         elif len(first_line) == 1:
@@ -35,7 +36,7 @@ def take_two_ints():
             return num1, num2
     except ValueError as e:
         print("Error parsing. Try again: ")
-        return take_two_ints() # Tail recursion? No stack overflow?
+        return take_two_ints()
 
 
 
